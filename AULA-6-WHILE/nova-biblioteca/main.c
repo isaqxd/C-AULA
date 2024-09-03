eea6d7ebec0404e2e67b9c11bf9717eecf279980
isaqxd-admin <isaque3356.ic@gmail.com>
2024-09-03 10:36:57 -0300

    brinquei

--- AULA-6-WHILE/nova-biblioteca/main.c
+++ AULA-6-WHILE/nova-biblioteca/main.c
@@ -8,31 +8,33 @@ main(){
     char letra;
 
     srand(time(NULL));
-    printf("\nDigite algo:\n");
-    scanf("%c", letra);
 
 
-    while (resultado<100){ 
+    while (1){
+    printf("aperta qualquer tecla para comecar, para sair precione [P] e tecle enter\n");
+    scanf("%c", &letra);
+
+    if (letra == 'p' || letra == 'P') {
+        break;
+    }
+    
         numero=(rand() % 21) + (-10);
-        printf("\nO seu numero e %d\n", numero);
+        printf("\nO seu numero e [%d]: ", numero);
 
     if (numero == 0) {
         resultado = resultado+1000;
-        printf("Voce ganhou 1000 pontos.\n");
+        printf("Voce ganhou 1000 pontos.:\n");
     }
-    if (numero >= -5 && numero <= 5){
+    else if (numero >= -5 && numero <= 5){
         resultado = resultado + 10;
-        printf("Voce ganhou 10 pontos.\n" );
-    }
-    else if ((numero >= -10) && (numero <= 10)){
-        resultado = resultado - 10;
-        printf("Voce perdeu 1 ponto.\n");
+        printf("Voce ganhou 10 pontos.:\n" );
     }
     else {
-        printf("Voce nao ganhou nada.\n");
+        resultado = resultado - 1;
+        printf("Voce perdeu 1 ponto.\n");
     }
+}
 
     printf("\nA sua pontuacao e %d\n", resultado);
-}
     return 0;
 }
