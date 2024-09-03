f1af064d14f605a4cf443f43c044cc38b45b8e62
isaqxd-admin <isaque3356.ic@gmail.com>
2024-09-03 09:29:19 -0300

    estutando

--- AULA-6-WHILE/nova-biblioteca/main.c
+++ AULA-6-WHILE/nova-biblioteca/main.c
@@ -3,16 +3,36 @@
 #include <time.h>
 
 main(){
-    int aleatorio;
-    int valmin= -10;
-    int valmax= 10;
+    int resultado = 0;
+    int numero;
+    char letra;
 
     srand(time(NULL));
+    printf("\nDigite algo:\n");
+    scanf("%c", letra);
 
-    for (int i = 0; i < 10; i++) {
-        aleatorio = rand();
-        aleatorio = valmin + aleatorio*(valmax - valmin)  + RAND_MAX/2;
-        printf("\n %d", aleatorio);
+
+    while (resultado<100){ 
+        numero=(rand() % 21) + (-10);
+        printf("\nO seu numero e %d\n", numero);
+
+    if (numero == 0) {
+        resultado = resultado+1000;
+        printf("Voce ganhou 1000 pontos.\n");
+    }
+    if (numero >= -5 && numero <= 5){
+        resultado = resultado + 10;
+        printf("Voce ganhou 10 pontos.\n" );
+    }
+    else if ((numero >= -10) && (numero <= 10)){
+        resultado = resultado - 10;
+        printf("Voce perdeu 1 ponto.\n");
+    }
+    else {
+        printf("Voce nao ganhou nada.\n");
     }
 
+    printf("\nA sua pontuacao e %d\n", resultado);
+}
+    return 0;
 }
