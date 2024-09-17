092c9120ef408677bdcbef44decea275a8388118
isaqxd <isaque3356@icloud.com>
2024-09-16 21:25:46 -0300

    teste

---
+++ AULA-10/Professor/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define QUANTIDADE (4)
+
+int main() {
+    int aletorio[QUANTIDADE];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < QUANTIDADE; i++) {
+        aleatorio[i] = rand();
+    }
+
+    for (int pos = 0; pos < (QUANTIDADE - 1); pos++) {
+        printf("\n A diff entre a posicao %d e %d e %d", pos, pos + 1, 
+        
+        aleatorio[pos+1] - aleatorio[pos]);
+    }
+}
